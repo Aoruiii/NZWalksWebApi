d42771e0ef7743b9f595c45625e04d935f79bc91
Aorui Tan <aorui.tan@Aoruis-MacBook-Pro.local>
2024-05-20 14:58:45 +1000

    update api document

--- NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
+++ NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a027b305473b134ad87423628369eb694511ea63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3853165e9f116e5d3268a78f7e5b6a821c9399d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
